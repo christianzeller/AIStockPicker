a13bd520a95869be9847d461f0ad544b286df4a7
Christian Zeller <christian-zeller@gmx.net>
2022-04-28 22:39:32 +0200

    stable

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,39 @@ def calcZScores(X):
         + 1.05 * X['Book Equity/TL']
     return Z
 
+@st.experimental_memo
+def getModelData(_ws):
+    model_meta = {}
+    model_version = {}
+    for model in Model.list(_ws):
+        # if model name starts with "AIInvestor-"
+        if model.name.startswith('AIInvestor-'):
+            # ensure metadata of only the latest model is loaded
+            if model.name in model_meta:
+                if model.version > model_version[model.name]:
+                    model_meta[model.name] = model.properties
+                    model_version[model.name] = model.version
+                    if model.run_id in model_meta:
+                        model_meta[model.name]['run_id'] = model.run_id
+            else:
+                model_meta[model.name] = model.properties
+                model_version[model.name] = model.version
+                if model.run_id in model_meta:
+                        model_meta[model.name]['run_id'] = model.run_id
+
+    return model_meta, model_version
+
+@st.experimental_memo
+def loadModel(_ws, model_name):
+    Model(_ws, model_name).download(target_dir='.', exist_ok=True)
+    pipeline = pickle.load(open("model.pkl", "rb" ))
+    return pipeline
+
+@st.experimental_memo
+def getPredictors(_ws, dsname):
+    X = Dataset.get_by_name(_ws, f'{dsname}-predictors_clipped').to_pandas_dataframe()
+    tickers = Dataset.get_by_name(_ws, f'{dsname}-tickers_dates').to_pandas_dataframe()
+    return X, tickers
 
 aml_auth = ServicePrincipalAuthentication(tenant_id=st.secrets['AML_TENANT_ID'],
                                                   service_principal_id=st.secrets['AML_PRINCIPAL_ID'],
@@ -57,55 +90,37 @@ fmp = FMP(st.secrets['FMP_API_KEY'])
 
 st.title('AI Stock Picker')
 
-# Select Model
-# -> Model Statistics
-# -> Stocks
-# -> Backtest (+ Trailing Stop Params + Report)
-# -> Benchmark Report (SPY vs. MSCI)
-# -> sector/industry split
-model_meta = {}
-model_version = {}
-for model in Model.list(ws):
-    # if model name starts with "AIInvestor-"
-    if model.name.startswith('AIInvestor-'):
-        # ensure metadata of only the latest model is loaded
-        if model.name in model_meta:
-            if model.version > model_version[model.name]:
-                model_meta[model.name] = model.properties
-                model_version[model.name] = model.version
-        else:
-            model_meta[model.name] = model.properties
-            model_version[model.name] = model.version
+model_meta, model_version = getModelData(ws)
+
 model_name = st.selectbox('Model', model_meta.keys())
 
 sections = {'explain':False, 'backtest':False}
 
-with st.expander('Model Metadata'):
-    # retrieve the available models from AzureML
-    # List registered models
-    h1,h2 = st.columns([3,3])
-    h1.subheader('Model Metrics')
-    h2.subheader('Stocks Selected')
-    c1,c2,c3,c4 = st.columns([1,1,1,3])
-    c1.metric('Records trained', model_meta[model_name]['records_trained'])
-    c2.metric('Features', model_meta[model_name]['no_features'])
-    c3.metric('Shares', model_meta[model_name]['unique_stocks'])
-    c1.metric('Years', model_meta[model_name]['years_trained'])
-    c2.metric('CAGR', str(round(float(model_meta[model_name]['cagr'])*100,1)) + "%")
-    c3.metric('Sharpe Ratio', round(float(model_meta[model_name]['sharpe']),2))
+# retrieve the available models from AzureML
+# List registered models
+h1,h2 = st.columns([3,3])
+h1.subheader('Model Metrics')
+h2.subheader('Stocks Selected')
+c1,c2,c3,c4 = st.columns([1,1,1,3])
+c1.metric('Records trained', model_meta[model_name]['records_trained'])
+c2.metric('Features', model_meta[model_name]['no_features'])
+c3.metric('Shares', model_meta[model_name]['unique_stocks'])
+c1.metric('Years', model_meta[model_name]['years_trained'])
+c2.metric('CAGR', str(round(float(model_meta[model_name]['cagr'])*100,1)) + "%")
+c3.metric('Sharpe Ratio', round(float(model_meta[model_name]['sharpe']),2))
 
-    stocks_benchmark = pd.DataFrame()
-    stock_selected = json.loads(model_meta[model_name]['stock_selection'])
-    for yr in stock_selected:
-        stocks_yr = pd.DataFrame(stock_selected[yr])
-        stocks_yr.columns = [yr]
-        stocks_benchmark = stocks_benchmark.join(stocks_yr, how="outer")
-    c4.table(stocks_benchmark)
+stocks_benchmark = pd.DataFrame()
+stock_selected = json.loads(model_meta[model_name]['stock_selection'])
+for yr in stock_selected:
+    stocks_yr = pd.DataFrame(stock_selected[yr])
+    stocks_yr.columns = [yr]
+    stocks_benchmark = stocks_benchmark.join(stocks_yr, how="outer")
+c4.table(stocks_benchmark)
 
-    # todo: Model Explainer
+# todo: Model Explainer
 
-    if st.button('Show Backtest'):
-        sections['backtest'] = True       
+if st.button('Show Backtest'):
+    sections['backtest'] = True       
 
 if sections['backtest'] == True:
     with st.expander('Model Backtest'):
@@ -154,14 +169,13 @@ if st.button('Pick Stocks'):
     status = st.empty()
     status.write("Loading Model...")
 
-    Model(ws, model_name).download(target_dir='.', exist_ok=True)
-    pipeline = pickle.load(open("model.pkl", "rb" ))
+    pipeline = loadModel(ws, model_name)
 
     status.write("Loading Data...")
 
     dsname = model_meta[model_name]['dataset']
-    X = Dataset.get_by_name(ws, f'{dsname}-predictors_clipped').to_pandas_dataframe()
-    tickers = Dataset.get_by_name(ws, f'{dsname}-tickers_dates').to_pandas_dataframe()
+
+    X, tickers = getPredictors(ws, dsname)
     
     bool_list = X['date'].between(
         pd.to_datetime(start_date, format='%Y-%m-%d', errors='coerce'),
@@ -184,91 +198,113 @@ if st.button('Pick Stocks'):
     
     Final_Predictions['Perf. Score'] = y_pred.reset_index(drop=True)\
                                     [zbl].reset_index(drop=True)
+    Final_Predictions[[x for x in X.columns]] = X[[x for x in X.columns]].reset_index(drop=True)[zbl].reset_index(drop=True)
     
     Final_Predictions = Final_Predictions.sort_values(by='Perf. Score', 
                                         ascending=False)\
-                                .head(20)
-                                #.reset_index(drop=True).head(20)
-
+                                .reset_index(drop=True).head(20)
     
     candidates = pd.DataFrame()
 
     for row in range(len(Final_Predictions)):
         ticker = Final_Predictions.iloc[row]['Ticker']
         profile = fmp.getFMPData(f'https://financialmodelingprep.com/api/v3/profile/{ticker}')[0]
-        candidate = pd.DataFrame({'Ticker': ticker, 'ISIN':profile['isin'],
-            'Name': profile['companyName'], 'Industry':profile['industry'],
-            'Sector':profile['sector'], 'Country':profile['country'],
-            'Price':profile['price'], 'DCF':profile['dcf'],
-            'Report Date':Final_Predictions.iloc[row]['Report Date'],
-            'Perf. Score':Final_Predictions.iloc[row]['Perf. Score']}, index=[0])
-        candidates = pd.concat([candidates, candidate])
-        candidates.dropna(subset=['ISIN'], inplace=True)
-        candidates.drop_duplicates(subset=['Name'], keep='first', inplace=True, ignore_index=True)
-    status.write(f'The top 10 stocks are:')
-    st.table(candidates.loc[:9, ['ISIN', 'Name', 'Price', 'DCF', 'Report Date', 'Perf. Score']])
+        
+        Final_Predictions.loc[Final_Predictions.index[row],'ISIN'] = profile['isin']
+        Final_Predictions.loc[Final_Predictions.index[row],'Name'] = profile['companyName']
+        Final_Predictions.loc[Final_Predictions.index[row],'Industry'] = profile['industry']
+        Final_Predictions.loc[Final_Predictions.index[row],'Sector'] = profile['sector']
+        Final_Predictions.loc[Final_Predictions.index[row],'Country'] = profile['country']
+        Final_Predictions.loc[Final_Predictions.index[row],'Price'] = profile['price']
+        Final_Predictions.loc[Final_Predictions.index[row],'DCF'] = profile['dcf']
+        candidates = Final_Predictions.copy()
+
+    print(Final_Predictions)
+        
+    #     candidate = pd.DataFrame({'Ticker': ticker, 'ISIN':profile['isin'],
+    #         'Name': profile['companyName'], 'Industry':profile['industry'],
+    #         'Sector':profile['sector'], 'Country':profile['country'],
+    #         'Price':profile['price'], 'DCF':profile['dcf']}, index=[0])
+    #     candidates = pd.concat([candidates, candidate])
+    # candidates = candidates.merge(Final_Predictions, how='left', on='Ticker')
+    # candidates.dropna(subset=['ISIN'], inplace=True)
+    # candidates.drop_duplicates(subset=['Name'], keep='first', inplace=True, ignore_index=True)
+    status.write('')
+    
+    predcol1, predcol2 = st.columns([1,1])
+    predcol1.write(f'The top 10 stocks are:')
+    predcol1.table(candidates.loc[:9, ['ISIN', 'Name']])
 
     year = str(start_date).split('-')[0]
     top_10_stocks = candidates['Ticker'].to_list()
     top_10_stocks = [item for item in top_10_stocks if not(pd.isnull(item)) == True]
     returns = fmp.getReturns(top_10_stocks, f"{str(int(year)+1)}-04-01", f"{str(int(year)+2)}-03-31")
 
-    st.subheader(f'Portfolio Performance since {str(int(year)+1)}-04-01')
-    st.pyplot(qs.plots.returns(returns, benchmark="SPY", show=False))
-    st.pyplot(qs.plots.log_returns(returns, benchmark="SPY", show=False))
-    st.pyplot(qs.plots.monthly_returns(returns, show=False))
-
-
-    # Statistics    
-    sector_count = candidates.loc[:9, ['Ticker', 'Sector']].groupby('Sector').count()
-    sector_count['Sector'] = sector_count.index
-    country_count = candidates.loc[:9, ['Ticker', 'Country']].groupby('Country').count()
-    country_count['Country'] = country_count.index
-
-    # labels = sector_count['Sector'].to_list()
-    # values = sector_count['Ticker'].to_list()
-    # fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5, title='Sector')])
-    # fig.update_layout(showlegend=False)
-    # st.plotly_chart(fig)
-
-    # labels = country_count['Country'].to_list()
-    # values = country_count['Ticker'].to_list()
-    # fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3, title='Country')])
-    # fig.update_layout(showlegend=False)
-    # st.plotly_chart(fig)
-
-    print(sector_count['Sector'].to_list())
-    print(country_count['Country'].to_list())
-    
-    fig =go.Figure(go.Sunburst(
-    labels=sector_count['Sector'].to_list(),
-    parents=country_count['Country'].to_list(),
-    values=[1,1,1,1,1,1,1,1,1,1],
-    ))
-    fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
-    st.plotly_chart(fig)
+    predcol2.write(f'Portfolio Performance since {str(int(year)+1)}-04-01')
+    predcol2.pyplot(qs.plots.returns(returns, benchmark="SPY", show=False))
+    #predcol2.pyplot(qs.plots.log_returns(returns, benchmark="SPY", show=False))
+    predcol2.pyplot(qs.plots.monthly_returns(returns, show=False))
 
 
     import shap
     import matplotlib.pyplot as plt
 
-    print(model_meta[model_name])
-    if model_meta[model_name]['model'] == 'KNeighborsRegressor':
-        explainer = shap.KernelExplainer(pipeline.named_steps['KNeighborsRegressor'])
-        observations = pipeline.named_steps['Power Transformer'].transform(X)
-    else:
+    if model_meta[model_name]['model'] != 'KNeighborsRegressor':
         explainer = shap.Explainer(pipeline)
         observations = X
-    shap_values = explainer.shap_values(observations)
 
-    shap.summary_plot(shap_values, X, show=False)
-    fig = plt.gcf()
-    st.pyplot(fig)
+#### LINE BY LINE
+    for candidate in range(10):
+        with st.expander(f'{candidate}: {candidates.iloc[candidate]["Name"]}'):
+            st.write(f'{candidates.iloc[candidate]["Name"]}')
+            st.write(f'{candidates.iloc[candidate]["ISIN"]}')
+            
+            if model_meta[model_name]['model'] != 'KNeighborsRegressor':
+                data_for_prediction = candidates.iloc[candidate,3:-7]
+                shap_values = explainer.shap_values(data_for_prediction)
+                plt.clf()
+                shap.force_plot(explainer.expected_value, shap_values, data_for_prediction, text_rotation=45, matplotlib=True, show=False)
+                fig = plt.gcf()
+                st.pyplot(fig)
 
-    data_for_prediction = X.iloc[0]
-    data_for_prediction_array = data_for_prediction.values.reshape(1, -1)
 
-    shap_values = explainer.shap_values(data_for_prediction)
-    shap.force_plot(explainer.expected_value, shap_values, data_for_prediction, matplotlib=True, show=False)
-    fig = plt.gcf()
-    st.pyplot(fig)
+#### SUMMARY PLOT
+
+    if model_meta[model_name]['model'] != 'KNeighborsRegressor':
+        plt.clf()
+        shap_values = explainer.shap_values(observations)
+        shap.summary_plot(shap_values, X, show=False)
+        fig = plt.gcf()
+        st.pyplot(fig)
+
+    # # Statistics    
+    # sector_count = candidates.loc[:9, ['Ticker', 'Sector']].groupby('Sector').count()
+    # sector_count['Sector'] = sector_count.index
+    # country_count = candidates.loc[:9, ['Ticker', 'Country']].groupby('Country').count()
+    # country_count['Country'] = country_count.index
+
+    # # labels = sector_count['Sector'].to_list()
+    # # values = sector_count['Ticker'].to_list()
+    # # fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5, title='Sector')])
+    # # fig.update_layout(showlegend=False)
+    # # st.plotly_chart(fig)
+
+    # # labels = country_count['Country'].to_list()
+    # # values = country_count['Ticker'].to_list()
+    # # fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3, title='Country')])
+    # # fig.update_layout(showlegend=False)
+    # # st.plotly_chart(fig)
+
+    # print(sector_count['Sector'].to_list())
+    # print(country_count['Country'].to_list())
+
+    # fig =go.Figure(go.Sunburst(
+    # labels=sector_count['Sector'].to_list(),
+    # parents=country_count['Country'].to_list(),
+    # values=[1,1,1,1,1,1,1,1,1,1],
+    # ))
+    # fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+    # st.plotly_chart(fig)
+
+
+    
